7be0ba8d0058d99c3c8ad2d926ee09deb800314b
devgresia <devgresia@gmail.com>
2023-07-19 13:40:46 +0545

    company website

--- app/company/profile/page.tsx
+++ app/company/profile/page.tsx
@@ -14,7 +14,8 @@ export default function Profile() {
     firstName: '',
     email: '',
     streetAddress: '',
-    country: ''
+    country: '',
+    website:'',
   });
 
   const [isFileUploadOpen, setIsFileUploadOpen] = useState(false);
@@ -80,6 +81,28 @@ export default function Profile() {
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder="ramnepali"
                     value={form.username}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* company website */}
+            <div className="sm:col-span-4">
+              <label htmlFor="website" className="block text-sm font-medium leading-6 text-gray-900">
+                Company Website
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm"></span>
+                  <input
+                    type="text"
+                    name="website"
+                    id="website"
+                    autoComplete="website"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="eg : www.gresianepal.com"
+                    value={form.website}
                     onChange={handleChange}
                   />
                 </div>
